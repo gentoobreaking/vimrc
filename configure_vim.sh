d519956fc767a79b14e6ba844fc6a1dec43a2ac6
David <david@DaviddeMacBook-Pro.local>
2021-12-22 17:12:46 +0800

    updated.

--- configure_vim.sh
+++ configure_vim.sh
@@ -3,16 +3,14 @@
 mkdir -p ~/.vim/colors
 
 # https://colorswat.ch/vim/schemes/solarized
-git clone https://github.com/altercation/vim-colors-solarized
-git clone https://github.com/vim-scripts/Risto-Color-Scheme.git
+[ ! -d vim-colors-solarized ] && git clone https://github.com/altercation/vim-colors-solarized
+[ ! -d Risto-Color-Scheme ] && git clone https://github.com/vim-scripts/Risto-Color-Scheme.git
 
 cp vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
 cp Risto-Color-Scheme/colors/risto.vim ~/.vim/colors/
 
-echo "
-syntax on
+echo "syntax on
 set t_Co=256
-#colorscheme solarized
-colorscheme risto
-" > ~/.vimrc
+colorscheme risto" > ~/.vimrc
 
+# --- END --- #
